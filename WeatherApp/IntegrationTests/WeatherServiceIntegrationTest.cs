ec924eca01ff5a7fadfaa0c0c97ae64db7115a1a
paulius27 <paulius.truikys@gmail.com>
2023-01-19 17:47:54 +0200

    Add integration tests for GetWeatherDescription.

--- WeatherApp/IntegrationTests/WeatherServiceIntegrationTest.cs
+++ WeatherApp/IntegrationTests/WeatherServiceIntegrationTest.cs
@@ -1,9 +1,14 @@
+using BL.Validation;
+using BL;
+using DAL;
 using Microsoft.Extensions.Configuration;
 
 namespace IntegrationTests
 {
     public class WeatherServiceIntegrationTest
     {
+        private IWeatherService _weatherService;
+
         [SetUp]
         public void Setup()
         {
@@ -12,12 +17,30 @@ namespace IntegrationTests
                 .Build();
 
             string apiKey = config["weather_api_key"] ?? throw new KeyNotFoundException("Weather API Key not found.");
+
+            var validationService = new Validation();
+            var weatherRepository = new WeatherRepository(apiKey);
+            _weatherService = new WeatherService(weatherRepository, validationService);
         }
 
         [Test]
-        public void Test1()
-        {   
-            Assert.Pass();
+        public async Task GetWeatherDescriptionByCityNameAsync_GetTemperatureFail_ErrorMessage()
+        {
+            string cityName = "?";
+
+            var weatherDescription = await _weatherService.GetWeatherDescriptionByCityNameAsync(cityName);
+
+            Assert.That(weatherDescription, Does.Match("Error: failed to get weather data \\((.*?)\\)\\."));
+        }
+
+        [Test]
+        public async Task GetWeatherDescriptionByCityNameAsync_GetTemperatureSuccess_WeatherDescription()
+        {
+            string cityName = "Paris";
+
+            var weatherDescription = await _weatherService.GetWeatherDescriptionByCityNameAsync(cityName);
+
+            Assert.That(weatherDescription, Does.Match("In " + cityName + " (\\d+(?:[\\.\\,]\\d{1,2})?) °C. ([^.]+)\\."));
         }
     }
 }
