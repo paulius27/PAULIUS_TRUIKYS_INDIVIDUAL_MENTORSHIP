3cded77e19fd3ca59c227df748d4f1f44bb63e3f
paulius27 <paulius.truikys@gmail.com>
2023-01-19 12:32:00 +0200

    Access API key in integration tests.

--- WeatherApp/IntegrationTests/WeatherServiceIntegrationTest.cs
+++ WeatherApp/IntegrationTests/WeatherServiceIntegrationTest.cs
@@ -1,15 +1,22 @@
+using Microsoft.Extensions.Configuration;
+
 namespace IntegrationTests
 {
-    public class Tests
+    public class WeatherServiceIntegrationTest
     {
         [SetUp]
         public void Setup()
         {
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets<WeatherServiceIntegrationTest>()
+                .Build();
+
+            string apiKey = config["weather_api_key"] ?? throw new KeyNotFoundException("Weather API Key not found.");
         }
 
         [Test]
         public void Test1()
-        {
+        {   
             Assert.Pass();
         }
     }
