edf721d0a89cb94d0fa5c3979d90b581970fef98
paulius27 <paulius.truikys@gmail.com>
2023-02-24 13:11:48 +0200

    Add CityService and update WeatherHistoryService to save weather data.

---
+++ WeatherApp/BL/ICityService.cs
@@ -0,0 +1,11 @@
+﻿using DAL.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public interface ICityService
+    {
+        Task<IEnumerable<City>> FindOrAddCities(params string[] cityNames);
+    }
+}
